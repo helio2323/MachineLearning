1c9eb66078bf2d1d822464dd588a626c00896e65
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-06-11 12:19:22 -0300

    11.06

--- Teste.py
+++ Teste.py
@@ -1,5 +1,17 @@
-contador = 1
+contador1 = 1
+positivo = 0
+negativo = 0
+numero = 0
 
-while contador <= 1000:
-    print(contador)
-    contador += 1
+while contador1 <= 5:
+    print("Informe um número")
+    numero = int(input())
+
+    if numero > 0:
+        positivo +=1
+    if numero < 0:
+        negativo += 1
+
+    contador1 += 1
+print("Total de número positivo é ", positivo)
+print("Total de número negativos é ", negativo)
